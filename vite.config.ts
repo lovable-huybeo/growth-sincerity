5356f72a66e79ebac5cc990918d5f32cc0ca6cf8
quochuyl6a2th-beep <quochuyl6a2th@gmail.com>
2025-11-16 19:09:19 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+   base: "/growth-sincerity/",
   server: {
     host: "::",
     port: 8080,
